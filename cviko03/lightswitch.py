1f6a5f979f5146ef6358e1d280d720294f2d2719
Ajordon11 <andrej.fula@gmail.com>
2021-03-07 19:40:27 +0100

    feat: create methods for writer and reader thread and small test

--- cviko03/lightswitch.py
+++ cviko03/lightswitch.py
@@ -1,4 +1,6 @@
-from fei.ppds import Mutex, Semaphore
+from time import sleep
+from random import randint
+from fei.ppds import Mutex, Semaphore, Thread
 
 
 class Lightswitch:
@@ -24,9 +26,40 @@ class Lightswitch:
 class SharedData:
     def __init__(self):
         self.switch = Lightswitch()
-        self.flag = Semaphore(1)
-        self.readers = 0
+        self.semaphore = Semaphore(1)
+        self.turnstile = Semaphore(1)
+
+
+def writer_thread(thread_id, shared):
+    while True:
+        shared.turnstile.wait()
+        sleep(randint(0, 10) / 10)
+        print(f'{thread_id} before wait...')
+        shared.semaphore.wait()
+        # priemerna dlzka zapisu - 0.5s
+        sleep(randint(0, 10) / 10)
+        shared.semaphore.signal()
+        shared.turnstile.signal()
+        print(f'{thread_id} after wait...')
+
+
+def reader_thread(thread_id, shared: SharedData):
+    while True:
+        shared.turnstile.wait()
+        shared.turnstile.signal()
+        sleep(randint(0, 10) / 10)
+        print(f'{thread_id} before wait...')
+        shared.switch.lock(shared.semaphore)
+        # priemerna dlzka citania - 0.5s
+        sleep(randint(0, 10) / 10)
+        shared.switch.unlock(shared.semaphore)
+        print(f'{thread_id} after wait...')
 
 
 if __name__ == '__main__':
-    pass
+    data = SharedData()
+    threads = []
+
+    for i in range(10):
+        threads.append(Thread(reader_thread, f'Reader n.{i}', data))
+    threads.append(Thread(writer_thread, f'Writer', data))
