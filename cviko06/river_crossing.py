a6c39772881b612c87c34e7329312e78ecfd4dfb
Ajordon11 <andrej.fula@gmail.com>
2021-03-28 18:17:02 +0200

    feat: initialize threads

--- cviko06/river_crossing.py
+++ cviko06/river_crossing.py
@@ -1,11 +1,13 @@
-from fei.ppds import Mutex
+from fei.ppds import Mutex, Semaphore, Thread
 
 
 class Shared:
     def __init__(self):
         self.police_count = 0
-        self.criminal_count = 0
+        self.criminals_count = 0
         self.mutex = Mutex()
+        self.police_sem = Semaphore(0)
+        self.criminals_sem = Semaphore(0)
 
 
 def board():
@@ -16,5 +18,25 @@ def row_boat():
     pass
 
 
-if __name__ == '__main__':
+def police(id):
     pass
+
+
+def criminal(id):
+    pass
+
+
+if __name__ == '__main__':
+    threads = list()
+    shared = Shared()
+    police_members = 4
+    criminals_members = 4
+
+    for police_id in range(0, police_members):
+        threads.append(Thread(police, police_id, shared))
+
+    for criminal_id in range(0, criminals_members):
+        threads.append(Thread(criminal, criminal_id, shared))
+
+    for t in threads:
+        t.join()
