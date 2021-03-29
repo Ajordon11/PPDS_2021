14d62847bca155529311ace167874abbe23c5d20
Ajordon11 <andrej.fula@gmail.com>
2021-03-29 11:27:05 +0200

    test: add numerous prints to check functionality

--- cviko06/river_crossing.py
+++ cviko06/river_crossing.py
@@ -1,4 +1,6 @@
-from fei.ppds import Mutex, Semaphore, Thread, print
+from time import sleep
+
+from fei.ppds import Mutex, Semaphore, Thread, print, randint
 
 
 class SimpleBarrier:
@@ -22,31 +24,42 @@ class Shared:
     def __init__(self):
         self.police_count = 0
         self.criminal_count = 0
-        self.mutex = Mutex()
+        self.mutex = Semaphore(1)
         self.police_sem = Semaphore(0)
         self.criminal_sem = Semaphore(0)
         self.barrier = SimpleBarrier(4)
+        self.cruise_num = 0
+        self.cruise_lock = Mutex()
 
 
-def board():
-    pass
+def board(crew):
+    # boarding takes some time
+    sleep(0.4 + randint(0, 10) / 10)
+    print(f'New passenger aboard: {crew}')
 
 
-def row_boat():
-    pass
+def row_boat(crew, captain, shared):
+    print(f'Boat {shared.cruise_num} is on the move, crew: {crew}, captain: {captain}')
+    print()
+    shared.cruise_lock.lock()
+    shared.cruise_num += 1
+    shared.cruise_lock.unlock()
+    sleep(0.8 + randint(0, 10) / 10)
 
 
 def police(id, shared):
     isCaptain = False
+    crew = ''
     while True:
-        shared.mutex.lock()
+        shared.mutex.wait()
         shared.police_count += 1
-        print(f'Policeman {id} ready to board.')
+        print(f'Policeman {id} ready to board the ship.')
 
         if shared.police_count == 4:
             isCaptain = True
             shared.police_count = 0
             shared.police_sem.signal(4)
+            crew = 'only police'
 
         elif shared.police_count == 2 and shared.criminal_count == 2:
             isCaptain = True
@@ -56,31 +69,35 @@ def police(id, shared):
             shared.criminal_count -= 2
             shared.criminal_sem.signal(2)
 
+            crew = 'mixed'
+
         else:
-            shared.mutex.unlock()
+            shared.mutex.signal()
 
         shared.police_sem.wait()
         print(f'Policeman {id} is boarding the ship.')
-        board()
+        board(f'Policeman {id}')
         shared.barrier.wait()
 
         if isCaptain:
             print(f'Policeman {id} is the captain of this cruise.')
-            row_boat()
-            shared.mutex.unlock()
+            row_boat(crew, f'Policeman {id}', shared)
+            shared.mutex.signal()
 
 
 def criminal(id, shared):
     isCaptain = False
+    crew = ''
     while True:
-        shared.mutex.lock()
+        shared.mutex.wait()
         shared.criminal_count += 1
-        print(f'Criminal {id} ready to board.')
+        print(f'Criminal {id} ready to board the ship.')
 
         if shared.criminal_count == 4:
             isCaptain = True
             shared.criminal_count = 0
             shared.criminal_sem.signal(4)
+            crew = 'only criminals'
 
         elif shared.police_count == 2 and shared.criminal_count == 2:
             isCaptain = True
@@ -90,18 +107,20 @@ def criminal(id, shared):
             shared.police_count -= 2
             shared.police_sem.signal(2)
 
+            crew = 'mixed'
+
         else:
-            shared.mutex.unlock()
+            shared.mutex.signal()
 
         shared.criminal_sem.wait()
         print(f'Criminal {id} is boarding the ship.')
-        board()
+        board(f'Criminal {id}')
         shared.barrier.wait()
 
         if isCaptain:
             print(f'Criminal {id} is the captain of this cruise.')
-            row_boat()
-            shared.mutex.unlock()
+            row_boat(crew, f'Criminal {id}', shared)
+            shared.mutex.signal()
 
 
 if __name__ == '__main__':
