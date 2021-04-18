ba8599361c09bea127946d49b43d88e56ef2bd9e
Ajordon11 <andrej.fula@gmail.com>
2021-04-18 15:50:34 +0200

    feat: apply code from UIM to call random dog API asynchronously, not working correctly yet

---
+++ cviko08/async_single_thread.py
@@ -0,0 +1,61 @@
+import asyncio
+import webbrowser
+
+import aiohttp
+import time
+
+
+async def task_get_dog(work_queue, tab_queue):
+    async with aiohttp.ClientSession() as session:
+        while not work_queue.empty():
+            url = await work_queue.get()
+            print(f'Getting new dog')
+            time_start = time.perf_counter()
+            async with session.get(url) as response:
+                data = await response.json()
+                await tab_queue.put(data.get('url'))
+            elapsed = time.perf_counter() - time_start
+            print(f'Getting dog elapsed time: {elapsed:.1f}')
+
+
+async def task_open_tab(work_queue, open_tab=False):
+    async with aiohttp.ClientSession() as session:
+        while not work_queue.empty():
+            url = await work_queue.get()
+            print(f'Opening tab, URL: {url}')
+            time_start = time.perf_counter()
+            async with session.get(url) as response:
+                await response.read()
+            if open_tab:
+                webbrowser.get('chrome').open(url)
+            elapsed = time.perf_counter() - time_start
+            print(f'Opening tab elapsed time: {elapsed:.1f}')
+
+
+async def main():
+    # Create the queue of work
+    default_url = 'https://random.dog/woof.json'
+    # Create the queue of work
+    dog_queue = asyncio.Queue()
+    tab_queue = asyncio.Queue()
+
+    for _ in range(5):
+        await dog_queue.put(default_url)
+
+    # Run the tasks
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        task_get_dog(dog_queue, tab_queue),
+        task_open_tab(tab_queue, True),
+    )
+    elapsed = time.perf_counter() - start_time
+    print(f'\nTotal elapsed time: {elapsed:.1f}')
+
+
+if __name__ == "__main__":
+    # use Google Chrome as web browser
+    webbrowser.register('chrome',
+                        None,
+                        webbrowser.BackgroundBrowser(
+                            'C://Program Files (x86)//Google//Chrome//Application//chrome.exe'))
+    asyncio.run(main())
